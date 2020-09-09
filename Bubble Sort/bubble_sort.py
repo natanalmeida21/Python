c27e601b432786139be70f9dfb50087d070c6714
Natan José de Almeida <natanalmeida158@hotmail.com>
2020-09-09 16:03:22 -0300

    Comentário
    Adicionei cometários para melhor entendimento da teoria

--- Bubble Sort/bubble_sort.py
+++ Bubble Sort/bubble_sort.py
@@ -1,10 +1,10 @@
 def bubble_sort(list):
     ordenado = False
-    while not ordenado:
+    while not ordenado: #Vai verificar se a lista estar ordenada, senão estiver enra no laço de repetição
         ordenado = True
-        for i in range(len(list) - 1):
-            if list[i] > list [i + 1]:
-                list[i], list[i + 1] = list[i + 1], list[i]
+        for i in range(len(list) - 1): #Como no Python começa por zero a posição vai ser o tamanho da lista menos um
+            if list[i] > list [i + 1]: #Comparação das posição 
+                list[i], list[i + 1] = list[i + 1], list[i] #Se condição acontecer, os números nos indicies vão trocar de lugar
                 ordenado = False
             print(list)
     return list
