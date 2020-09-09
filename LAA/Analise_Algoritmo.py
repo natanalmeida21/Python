5e10805db0af1e4f68b3aeac2fe6f1a7545c68cc
Natan José de Almeida <natanalmeida158@hotmail.com>
2020-09-09 14:49:03 -0300

    Váriavies
    Criação de variávies com as fórmulas de insercão e intercalção

--- LAA/Analise_Algoritmo.py
+++ LAA/Analise_Algoritmo.py
@@ -1,1 +1,6 @@
-print("Olá, Mundo")
+import math
+n = int(input("Quantos elementos serão ordenados? "))
+m = int(input("Quantas instruções por segundo? "))
+insercao = ((n ** 2) * 2) / m
+intercalacao = ((math.log10(n)) * n) / m
+print(insercao)
