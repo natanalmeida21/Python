8200191a549f97d7218ee9ee592b2c1149d7ae5f
Natan José de Almeida <natanalmeida158@hotmail.com>
2020-09-09 15:00:31 -0300

    Menu
    Criação de um menu com as opções para analisar o algoritmo.

--- LAA/Analise_Algoritmo.py
+++ LAA/Analise_Algoritmo.py
@@ -8,4 +8,12 @@ def intercalacao(a, b):
 
 n = int(input("Quantos elementos serão ordenados? "))
 m = int(input("Quantas instruções por segundo? "))
-print(f"O resuldado por inserção é {insercao(n,m)}")
+a = int(input("Escolha um opção: 1- Inserção 2- Intercalação 3- Ambos"))
+if a == 1:
+    print(f"O resultado por inserção é {insercao(n, m)}")
+elif a == 2:
+    print(f"O resultado por intercalação é {intercalacao(n, m)}")
+elif a == 3:
+    print(f"I resulto por inserção é {insercao(n, m)} e por intercalação é {intercalacao(n, m)}")
+else:
+    print("Essa opção é inválida")
