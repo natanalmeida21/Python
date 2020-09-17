7c5562ecc7fc14a502330d0788d7c9d9bfaf4f31
Natan José de Almeida <natanalmeida158@hotmail.com>
2020-09-17 10:53:06 -0300

    Criação de Menus
    Configuração de caminhos

--- Analise_Algoritmo/Busca.py
+++ Analise_Algoritmo/Busca.py
@@ -1,3 +1,5 @@
+import time
+
 def busca_binaria(lista, item):
     esquerda, direita = 0, len(lista) - 1
     while esquerda <= direita:
@@ -10,6 +12,42 @@ def busca_binaria(lista, item):
             esquerda = meio + 1
     return -1
 
+def busca_sequencial(lista, item):
+    for i in range(len(lista)):
+        if lista[i] == item:
+            return i
 
+def insert_sort(lista):
+    for i in range(1, len(lista)):
+        j = i - 1
+        while True:
+            if j >= 0 and lista[j] > lista[j + 1]:
+                chave = lista[j]
+                lista[j] = lista[j + 1]
+                lista[j + 1] = chave
+                j -= 1
+            else:
+                break
+    return lista
 
-vetor = [0, 10, 20, 30, 40, 50, 60, 70]
+vetor = []
+print("*" * 16, "Menu", "*" * 16)
+print("1- Adicionar Inteiros no vetor\n2- Adicionar Strings no vetor")
+while True:
+    opcao = int(input("Informe a sua opção: "))
+    if opcao == 1:
+        elementos = int(input("Informe a quantidade de elementos: "))
+        for c in range(elementos):
+            vetor.append(int(input(f"Qual o elemento(int) para a posição {c}: ")))
+        break
+    elif opcao == 2:
+        elementos = int(input("Informe a quantidade de elementos: "))
+        for c in range(elementos):
+            vetor.append(str(input(f"Qual o elemento(str) para a posição {c}: ")))
+        break
+    else:
+        print("Opção inválida")
+
+print(f"Seu vetor é {vetor} e ordenado fica {insert_sort(vetor)}")
+print("*" * 16, "Escolha a sua busca", "*" * 16)
+print("1- Busca binária\n2- Busca sequencial\n3- Ambos")
