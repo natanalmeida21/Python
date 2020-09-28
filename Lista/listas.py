7278bd3e0b02291a54a13cd7884ab9dff3d5cec8
Natan José de Almeida <natanalmeida158@hotmail.com>
2020-09-28 16:58:30 -0300

    Teoria

---
+++ Lista/listas.py
@@ -0,0 +1,16 @@
+"""
+Listas
+
+Listas em Python funcional como vetores/matrizes (arrays) em outras linguagens, com a diferenças de serem DINÂMICOS e também de podermos colcoar QUALQUER tipo de dado.
+
+Linguagens C/Java: Arrays
+    - Possuem tamanho e tipo de dado fixo:
+    Ou seja, nestas linguagens se você criar um array do tipo int e com tamanho 5, este array será sempre do tipo inteiro e poderá ter SEMPRE no máximo 5 valores.
+
+Já em Python:
+-Dinâmico: Não possuem tamanho fixo: Ou seja, podemos criar a lista e simplesmente ir adicionando elementos;
+- Qualquer tipo de dado: Não posseum tipo de dado fixo: Ou seja, podemos colocar qualquer tipo de dado;
+
+As listas em Python são representadas por colchetes: []
+"""
+
